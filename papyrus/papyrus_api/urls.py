e0c90aaa0a6d2ed1e357488a85525d699c1dbdb9
Yuko <tanchho.limbu25@gmail.com>
2025-09-18 15:35:55 +0545

    Added DefaultRouter to accomodate modelviewsets

--- papyrus/papyrus_api/urls.py
+++ papyrus/papyrus_api/urls.py
@@ -1,8 +1,16 @@
 from django.urls import path
-from .views import get_users, create_user, user_detail
+from . import views
+from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-    path('users/', get_users, name='get_users'),
-    path('users/create', create_user, name='create_user'),
-    path('users/<int:pk>',user_detail, name='user_detail'),
+    # path('users/', views.UserListAPIView.as_view()),
+    # path('users/create', views.create_user),
+    # path('users/<int:user_id>',views.UserDetailAPIView.as_view()),
+    # path('books/info',views.BookInfoAPIView.as_view()),
 ]
+
+router = DefaultRouter()
+router.register('users', views.UserViewSet)
+router.register('books', views.BookViewSet)
+router.register('reviews', views.ReviewViewSet)
+urlpatterns += router.urls
