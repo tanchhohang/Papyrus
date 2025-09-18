ebeae703dd98df80f94c601e52911bfa67a52fb8
Yuko <tanchho.limbu25@gmail.com>
2025-09-18 15:33:12 +0545

    Admin configs  for book and review models

--- papyrus/papyrus_api/admin.py
+++ papyrus/papyrus_api/admin.py
@@ -1,3 +1,14 @@
 from django.contrib import admin
-
+from papyrus_api.models import Book, Review, User
 # Register your models here.
+
+class ReviewInline(admin.TabularInline):
+    model = Review
+
+class BookAdmin(admin.ModelAdmin):
+    inlines=[
+        ReviewInline
+    ]
+
+
+admin.site.register(Book, BookAdmin)
