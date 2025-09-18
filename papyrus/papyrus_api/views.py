607c6e2ab7c5660a6c04ca3aa9fc4e2f1182dae3
Yuko <tanchho.limbu25@gmail.com>
2025-09-18 15:35:14 +0545

    Changed views from function-based views to class-based views

--- papyrus/papyrus_api/views.py
+++ papyrus/papyrus_api/views.py
@@ -1,71 +1,38 @@
 from rest_framework.decorators import api_view, action
 from rest_framework.response import Response
-from rest_framework import viewsets, permissions, status
+from rest_framework import viewsets, permissions, status, generics
 from django.contrib.auth.models import User
 from .models import Book, Review
-from .serializer import UserSerializer, BookSerializer, ReviewSerializer
+from .serializer import UserSerializer, BookSerializer, ReviewSerializer, BookInfoSerializer
 
 #USERS
-@api_view(["GET"])
-def get_users(request):
-    users = User.objects.all()
-    serializer = UserSerializer(users,many=True)
-    return Response(serializer.data)
 
-@api_view(["POST"])
-def create_user(request):
-    serializer = UserSerializer(data=request.data)
-    if serializer.is_valid():
-        serializer.save()
-        return Response(serializer.data, status = status.HTTP_201_CREATED)
-    return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-
-@api_view(["GET","PUT","DELETE"])
-def user_detail(request,pk):
-    try:
-        user = User.objects.get(pk=pk)
-    except User.DoesNotExist:
-        return Response(status=status.HTTP_404_NOT_FOUND)
+class UserViewSet(viewsets.ModelViewSet):
+    queryset = User.objects.all()
+    serializer_class = UserSerializer
     
-    if request.method == 'GET':
-        serializer = UserSerializer(user)
-        return Response(serializer.data)
-    
-    elif request.method == 'PUT':
-        serializer = UserSerializer(user, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data, status = status.HTTP_200_OK)
-        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
+# class UserListAPIView(generics.ListAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
 
-    elif request.method == 'DELETE':
-        user.delete()
-        return Response(status=status.HTTP_204_NO_CONTENT)
-
+# class UserDetailAPIView(generics.RetrieveAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     lookup_url_kwarg = 'user_id'
 
 #BOOKS
+class BookInfoAPIView(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
-    serializer = BookSerializer
+    serializer_class = BookSerializer
 
 #REVIEW
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
-    serializer = ReviewSerializer
+    serializer_class = ReviewSerializer
 
-# @api_view(['GET'])
-# def get_books(request):
-#     books = Book.objects.all()
-#     serializer = BookSerializer(books, many=True)
-#     return Response(serializer.data)
 
-# @api_view(["POST"])
-# def add_books(request):
-#     serializer = BookSerializer(data=request.data)
-#     if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data, status = status.HTTP_201_CREATED)
-#     return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-    
-# @api_view(["GET","PUT","DELETE"])
-# def book_details(request):
