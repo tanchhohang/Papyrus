17cae60e8fa301fab53c2d607a48a73d8be141b2
Yuko <tanchho.limbu25@gmail.com>
2025-09-18 15:34:00 +0545

    Minor edits. TODO: Utilize uuid instead of default id

--- papyrus/papyrus_api/models.py
+++ papyrus/papyrus_api/models.py
@@ -1,12 +1,9 @@
+import uuid
 from django.db import models
 from django.contrib.auth.models import User
 
+#TODO use of UUID inetead of default id
 # Create your models here.
-class User(User):
-    # TODO: Implement user later
-    pass
-
-
 class Book(models.Model):
     title = models.CharField(max_length=255)
     author = models.CharField(max_length=100)
@@ -24,7 +21,8 @@ class Book(models.Model):
 class Review(models.Model):
     book = models.ForeignKey(Book, related_name='reviews',on_delete=models.CASCADE)
     user = models.ForeignKey(User, related_name='reviews',on_delete=models.CASCADE)
-    rating = models.DecimalField(max_digits=3, decimal_places=2)
+    rating = models.DecimalField(max_digits=3, decimal_places=1)
+    comment = models.CharField(max_length=500, blank=True, default='Good Book')
     date = models.DateTimeField(auto_now_add=True)
 
     def __str__(self):
