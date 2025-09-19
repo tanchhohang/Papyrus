010481e6810bbb51eaf071483a0cbb02fa0033ed
Yuko <tanchho.limbu25@gmail.com>
2025-09-19 23:38:35 +0545

    Included url for UserReviewViewSet along with basename

--- papyrus/papyrus_api/urls.py
+++ papyrus/papyrus_api/urls.py
@@ -3,14 +3,12 @@ from . import views
 from rest_framework.routers import DefaultRouter
 
 urlpatterns = [
-    # path('users/', views.UserListAPIView.as_view()),
-    # path('users/create', views.create_user),
-    # path('users/<int:user_id>',views.UserDetailAPIView.as_view()),
-    # path('books/info',views.BookInfoAPIView.as_view()),
+
 ]
 
 router = DefaultRouter()
 router.register('users', views.UserViewSet)
 router.register('books', views.BookViewSet)
 router.register('reviews', views.ReviewViewSet)
+router.register('user-reviews', views.UserReviewViewSet, basename='user-reviews')
 urlpatterns += router.urls
