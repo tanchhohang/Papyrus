f14a2f7d0219db592a644a90199903659eda80d0
Yuko <tanchho.limbu25@gmail.com>
2025-09-19 23:39:21 +0545

    Implemented dynamic filtering and permissions

--- papyrus/papyrus_api/views.py
+++ papyrus/papyrus_api/views.py
@@ -1,30 +1,19 @@
-from rest_framework.decorators import api_view, action
-from rest_framework.response import Response
-from rest_framework import viewsets, permissions, status, generics
+from rest_framework import viewsets, generics
+from rest_framework.permissions import IsAuthenticated
 from django.contrib.auth.models import User
 from .models import Book, Review
 from .serializer import UserSerializer, BookSerializer, ReviewSerializer, BookInfoSerializer
 
 #USERS
-
 class UserViewSet(viewsets.ModelViewSet):
     queryset = User.objects.all()
     serializer_class = UserSerializer
-    
-# class UserListAPIView(generics.ListAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
 
-# class UserDetailAPIView(generics.RetrieveAPIView):
-#     queryset = User.objects.all()
-#     serializer_class = UserSerializer
-#     lookup_url_kwarg = 'user_id'
 
 #BOOKS
 class BookInfoAPIView(generics.ListAPIView):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
-
 
 class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
@@ -34,5 +23,14 @@ class BookViewSet(viewsets.ModelViewSet):
 class ReviewViewSet(viewsets.ModelViewSet):
     queryset = Review.objects.all()
     serializer_class = ReviewSerializer
+    
 
+class UserReviewViewSet(viewsets.ModelViewSet):
+    queryset = Review.objects.all()
+    serializer_class = ReviewSerializer
+    permission_classes = [IsAuthenticated]
 
+    def get_queryset(self):
+        user = self.request.user
+        qs = super().get_queryset()
+        return qs.filter(user=self.request.user)
