8bff3e5000f2c0055ee69f606d075f730d65bf35
Yuko <tanchho.limbu25@gmail.com>
2025-09-23 22:10:03 +0545

    Minor changes

--- papyrus/papyrus_api/serializer.py
+++ papyrus/papyrus_api/serializer.py
@@ -5,25 +5,44 @@ from .models import Book, Review
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['id', 'username', 'email', 'date_joined']
+        fields = [
+            'id', 
+            'username', 
+            'email', 
+            'date_joined'
+            ]
 
 class ReviewSerializer(serializers.ModelSerializer):
     user = serializers.StringRelatedField(read_only=True)
+
     # user = UserSerializer(many=True, read_only=True)
     class Meta:
         model = Review
-        fields = ['id','book','user','rating','comment','date']
+        fields = [
+            'id',
+            'book',
+            'user',
+            'rating',
+            'comment',
+            'date'
+            ]
 
-        def validate_rating(self,value):
-            if value > 10 or value < 0:
-                raise serializers.ValidationError('Ratings must be between 0 - 10')
-            return value
+    def validate_rating(self,value):
+        if value > 10 or value < 0:
+            raise serializers.ValidationError('Ratings must be between 0 - 10')
+        return value
 
 class BookSerializer(serializers.ModelSerializer):
     avgrating = serializers.FloatField(read_only=True)
     class Meta:
         model = Book
-        fields = ['id','title','author','description','avgrating']
+        fields = [
+            'id',
+            'title',
+            'author',
+            'description',
+            'avgrating'
+            ]
 
 class BookInfoSerializer(serializers.Serializer):
     books = BookSerializer(many=True)
