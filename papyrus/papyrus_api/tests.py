d0fcdb0e3ef063c3b5d6a3c95a10676c8ca5beda
Yuko <tanchho.limbu25@gmail.com>
2025-09-25 21:39:36 +0545

    Added initial APITestCase for Book: get, update, and delete tests

--- papyrus/papyrus_api/tests.py
+++ papyrus/papyrus_api/tests.py
@@ -1,24 +1,45 @@
-from django.test import TestCase
 from .models import Review, User, Book
 from django.urls import reverse
+from rest_framework.test import APITestCase
+from rest_framework import status
 
 # Create your tests here.
-class UserReviewTestCase(TestCase):
+class BookAPITestCase(APITestCase):
     def setUp(self):
-        self.user1 = User.objects.create_user(username='user1', email='a@b.com',password='temp')
-        self.user2 = User.objects.create_user(username='user2', email='d@b.com',password='temp')
+        self.admin_user = User.objects.create_superuser(username='admin',password='adminpass')
+        self.normal_user = User.objects.create_user(username='user1',email='user@g.com',password='userpass')
+        self.book = Book.objects.create(
+            title ='Test Book',
+            author = 'TestAuthor',
+            description = 'Test Description'
+        )
 
-        self.book = Book.objects.create(title='Test Book', author='John Doe', description='Test description')
+        self.book_list_url = reverse('book-list')
+        self.book_detail_url = reverse('book-detail', kwargs={'pk': self.book.pk})
 
-        Review.objects.create(book=self.book, user=self.user1, rating=8.5, comment="Great book")
-        Review.objects.create(book=self.book, user=self.user2, rating=7.5, comment="Not bad")
+    def test_get_book(self):
+        response = self.client.get(self.book_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(response.data['title'], self.book.title)
 
-    def test_user_review_endpoint_retrieves_only_authenticated_user_reviews(self):
-        user = User.objects.get(username = 'user1')
+    def test_unauthorized_update_book(self):
+        data = {"title": "Updated Title"}
+        response = self.client.put(self.book_detail_url, data, format='json')
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-        self.client.force_login(user)
-        response = self.client.get(reverse('user-reviews-list'))
+    def test_unauthorized_delete_book(self):
+        response = self.client.delete(self.book_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_401_UNAUTHORIZED)
 
-        assert response.status_code == 200
-        data = response.json()
-        print(data)
+    def test_only_admins_can_delete_product(self):
+        # test normal user cannot delete
+        self.client.login(username='user1',password='userpass')
+        response = self.client.delete(self.book_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_403_FORBIDDEN)
+        self.assertTrue(Book.objects.filter(pk=self.book.pk).exists())
+
+        #test admin user can delete
+        self.client.login(username='admin',password='adminpass')
+        response = self.client.delete(self.book_detail_url)
+        self.assertEqual(response.status_code, status.HTTP_204_NO_CONTENT)
+        self.assertFalse(Book.objects.filter(pk=self.book.pk).exists())
