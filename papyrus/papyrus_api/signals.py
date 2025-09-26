bb5dcb96d046df9604c197a2b54fff3f9aa159ee
Yuko <tanchho.limbu25@gmail.com>
2025-09-26 22:53:43 +0545

    Initial commit: Added Django signals for cache invalidation

---
+++ papyrus/papyrus_api/signals.py
@@ -0,0 +1,11 @@
+from django.db.models.signals import post_save, post_delete
+from django.dispatch import receiver
+from .models import Book
+from django.core.cache import cache
+
+
+@receiver([post_save,post_delete],sender=Book)
+def invalidate_product_cache(sender,instance, **kwargs):
+    print("Clearing book cache")
+
+    cache.delete_pattern('*book_list*')
